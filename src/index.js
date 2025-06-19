16588f136944f07a5777a8394272955cdbcd48a4
nguyenmaily <nguyenmaily140703@gmail.com>
2025-06-19 15:04:08 +0700

    update final

--- src/index.js
+++ src/index.js
@@ -28,6 +28,6 @@ app.use('/api/music', musicRoutes);
 // Error handler
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, '0.0.0.0',() => {
+  console.log(`Server is running on port ${PORT}and accessible from network`);
 });
